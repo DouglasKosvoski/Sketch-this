ed5d855a356d8e350c39eadfc489811c2184ab07
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-04-25 16:03:05 -0300

    Update: Organizado em mais pastas

---
+++ FrontEnd Projeto Integrador/src/php/include/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>Termos de uso</p>
+</footer>
---
+++ FrontEnd Projeto Integrador/src/php/include/header.php
@@ -0,0 +1,13 @@
+<head>
+    <title>Projeto Integrador</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Site feito para o projeto integrador ENG. SOFT/BD/PROG II.">
+    <meta name="keywords" content="Chat, Conversa, Anônimo">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Sarabun&display=swap" rel="stylesheet">
+
+    <script src="./js/javascript.js"></script>
+</head>
