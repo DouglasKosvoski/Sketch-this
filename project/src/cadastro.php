f35c0604940ed5176ec6cea748d4b6bab9797197
Guilherme <49656211+SWE3T@users.noreply.github.com>
2021-05-05 14:03:17 -0300

    Add files via upload

---
+++ project/src/cadastro.php
@@ -0,0 +1,33 @@
+<?php include './php/include/header.php';?>
+
+<body class="telaLogin">
+    <header>
+        <a href="index.html"><img src="../img/Voltar.svg" id="voltarLogin"></a>
+    </header>
+    <div>
+        <div class="avatar" onclick="avatares()">
+            <div class="img-avatar-wrapper" id="avat">
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-3bbf058328c6c9d6fc.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-3c3bb41fba2307b726.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-6980dfde77790611d8.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-16070911a0ea440e79.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-16070911a0ea440e79.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-fbb3f9d5e6be92b52c.png" onclick="selectAvatar()"></div>
+                <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
+                
+            </div>
+        </div>
+        <form>
+            <input type="nick" name="nick_field" size="37" maxlength="50" placeholder="Nickname" id="id_nick" class="campo-nickname" autocomplete="off" required>
+            <input type="email" name="email_field" size="37" maxlength="50" placeholder="E-mail" id="id_email" class="campo-email" autocomplete="off" required>
+            <input type="password" name="password_field" size="37" maxlength="45" placeholder="Senha" id="password" class="campo-senha" required>
+            <input type="submit" value="Cadastrar" onclick="login()" class="entrar">
+        </form>
+        <div class="termos">*Ao cadastrar você concorda com os termos de uso</div>
+    </div>
+ </body>
+
+ <?php include './php/include/footer.php';?>
