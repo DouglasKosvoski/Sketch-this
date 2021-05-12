12714285fd52c34ab337178bb41b5a1a8b89ecd9
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-05-12 11:47:37 -0300

    Update: Exibe todas as imagens contidas na pasta 'img/avatares' para serem selecionadas durante o cadastro

--- project/src/php/cadastro.php
+++ project/src/php/cadastro.php
@@ -24,18 +24,15 @@ else { ?>
 
     <div class="avatar" onclick="avatares()">
       <div class="img-avatar-wrapper" id="avat">
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-3bbf058328c6c9d6fc.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-3c3bb41fba2307b726.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-6980dfde77790611d8.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-16070911a0ea440e79.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-16070911a0ea440e79.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-fbb3f9d5e6be92b52c.png" onclick="selectAvatar()"></div>
-        <div><img id="img-avatar" src="..\img\avatares\Multiavatar-0cd252556ba6fd4b02.png" onclick="selectAvatar()"></div>
+        <?php
+        $dir = "../img/avatares/";
+        $images = glob($dir."*.png");
+        foreach($images as $image) {
+          echo '<div><img src="'.$image.'" id="'.$image.'" style="margin:10%; max-width:80%;height:auto;" onclick="selectAvatar()"></div>';
+        } ?>
       </div>
     </div>
+
     <form method="POST" action="">
       <input type="nick" name="nick_field" size="37" maxlength="50" placeholder="Nickname" id="id_nick" class="campo-nickname" autocomplete="off" required>
       <input type="email" name="email_field" size="37" maxlength="50" placeholder="E-mail" id="id_email" class="campo-email" autocomplete="off" required>
@@ -43,6 +40,6 @@ else { ?>
       <input type="submit" value="Cadastrar" class="entrar">
     </form>
 
-    <div class="termos">*Ao cadastrar você concorda com os termos de uso</div>
-  <?php }?>
+  <div class="termos">*Ao cadastrar você concorda com os termos de uso</div>
+<?php }?>
 </body>
